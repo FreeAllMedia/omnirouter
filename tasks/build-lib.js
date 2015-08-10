18a76287df5bb42bda2f6463de775ea76dc00bbc
nicosommi <nicosommi@gmail.com>
2015-08-10 18:56:26 -0300

    watch implementation

--- tasks/build-lib.js
+++ tasks/build-lib.js
@@ -3,7 +3,7 @@ import babel from "gulp-babel";
 
 import paths from "../paths.json";
 
-gulp.task("build-lib", ["build-resources"], () => {
+gulp.task("build-lib", () => {
 	return gulp.src(paths.source.lib)
 		.pipe(babel())
 		.pipe(gulp.dest(paths.build.directories.lib));
